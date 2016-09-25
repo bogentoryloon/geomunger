8b2fe0bbdc40a83b418d3f07931c8cccb362653b
U-NEWPC\John <john.cattanach@gmail.com>
2016-09-25 20:39:04 +0100

    filterring now

--- src/test/java/ie/jtc/services/TestFileReader.java
+++ src/test/java/ie/jtc/services/TestFileReader.java
@@ -4,7 +4,10 @@ import static org.junit.Assert.*;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,10 +22,19 @@ import ie.jtc.model.GPLocation;
 public class TestFileReader {
 	@Autowired
 	FileAccessor fileReader;
+	Logger log = Logger.getLogger(TestFileReader.class);
 	@Test
 	public void testReadPCRSFile() throws JsonParseException, IOException {
 		List<GPLocation> gps = fileReader.readPCRSFile("UGP_GPlist_by_LHO.json");
 		assertEquals(2749,gps.size() );
 	}
-
+	// see if I can use this lambda thing
+	@Test
+	public void testLambda() throws JsonParseException, IOException {
+		List<GPLocation> gps = fileReader.readPCRSFile("UGP_GPlist_by_LHO.json");
+		
+		List<GPLocation> dubGps = gps.stream().filter(p -> p.getAddress().indexOf("DUBLIN")!=-1).collect(Collectors.toList());
+		log.info("Dublin GPs = "+dubGps.size());
+		assertEquals(304,dubGps.size());
+	}
 }
